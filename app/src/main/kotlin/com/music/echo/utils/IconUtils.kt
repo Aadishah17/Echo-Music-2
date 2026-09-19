c9d3dabe6b4c3ccd692a60481bcde762ae089321
Aditya <adityadav9532@gmail.com>
2026-09-20 02:36:25 +0530

    feat(ui): add new app icons to appearance settings
    - Features: Added 'Echo Cat', 'Eko', and 'Wierd Cat' alternative app icons
    - Logic: Updated IconUtils and AppIconSettingsScreen to handle the new enums
    - System: Registered new dynamic <activity-alias> components in AndroidManifest.xml
    - Chore: Cleaned up imported icon source folders

--- app/src/main/kotlin/com/music/echo/utils/IconUtils.kt
+++ app/src/main/kotlin/com/music/echo/utils/IconUtils.kt
@@ -11,7 +11,10 @@ enum class AppIconType(val value: Int) {
   CAT(3),
   CRAZY_BLUE(4),
   POOKIE(5),
-  SKY(6)
+  SKY(6),
+  ECHO_CAT(7),
+  EKO(8),
+  WIERD_CAT(9)
 }
 
 object IconUtils {
@@ -24,6 +27,9 @@ object IconUtils {
     val crazyBlue = ComponentName(context, "echo.music.iad1tya.MainActivityCrazyBlue")
     val pookie = ComponentName(context, "echo.music.iad1tya.MainActivityPookie")
     val sky = ComponentName(context, "echo.music.iad1tya.MainActivitySky")
+    val echoCat = ComponentName(context, "echo.music.iad1tya.MainActivityEchoCat")
+    val eko = ComponentName(context, "echo.music.iad1tya.MainActivityEko")
+    val wierdCat = ComponentName(context, "echo.music.iad1tya.MainActivityWierdCat")
 
     pm.setComponentEnabledSetting(
       dynamic,
@@ -64,6 +70,24 @@ object IconUtils {
     pm.setComponentEnabledSetting(
       sky,
       if (iconType == AppIconType.SKY) PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+      else PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+      PackageManager.DONT_KILL_APP
+    )
+    pm.setComponentEnabledSetting(
+      echoCat,
+      if (iconType == AppIconType.ECHO_CAT) PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+      else PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+      PackageManager.DONT_KILL_APP
+    )
+    pm.setComponentEnabledSetting(
+      eko,
+      if (iconType == AppIconType.EKO) PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+      else PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+      PackageManager.DONT_KILL_APP
+    )
+    pm.setComponentEnabledSetting(
+      wierdCat,
+      if (iconType == AppIconType.WIERD_CAT) PackageManager.COMPONENT_ENABLED_STATE_ENABLED
       else PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
       PackageManager.DONT_KILL_APP
     )
